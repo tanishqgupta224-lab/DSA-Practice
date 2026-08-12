0ca9ff9651639ad3257562073ca2a64c5b57de9e
Tanishq gupta <tanishqgupta224@gmail.com>
2026-08-12 17:58:40 +0530

    Create question1.cpp

---
+++ Design-pattern/question1.cpp
@@ -0,0 +1,28 @@
+Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+*
+**
+***
+****
+*****
+
+
+#include<iostream>
+using namespace std;
+void pat1(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+
+        }
+    cout<<endl;
+    }
+
+}
+int main(){
+    int val;
+    cout<<"enter the  value";
+    cin>>val;
+    pat1(val);
+    return 0;
+    
+}
