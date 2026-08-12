74b3047b1f3217cb2c4f451c8055bc607c513c96
Tanishq Gupta <tanishqgupta292005@gmail.com>
2026-08-12 18:36:51 +0530

    solved patter question 1

--- Design-pattern/question1.cpp
+++ Design-pattern/question1.cpp
@@ -1,9 +1,9 @@
-Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
-*
-**
-***
-****
-*****
+// Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+// *
+// **
+// ***
+// ****
+// *****
 
 
 #include<iostream>
