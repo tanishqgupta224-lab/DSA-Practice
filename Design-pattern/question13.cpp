e9f58141c9311c2ccae15a99b3d03262b958be48
Tanishq Gupta <tanishqgupta224@gmail.com>
2026-08-18 20:23:35 +0530

    practice

---
+++ Design-pattern/question13.cpp
@@ -0,0 +1,18 @@
+// 12345
+// 1234
+// 123
+// 12
+// 1
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5;
+    for(int i=0;i<n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<j;
+        }
+    cout<<endl;
+    }
+    return 0;
+}
