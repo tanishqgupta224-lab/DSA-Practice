9cefd9dd2b089d27481a730f278f9040f61fb802
Tanishq Gupta <tanishqgupta224@gmail.com>
2026-08-29 03:53:17 +0530

    practice

---
+++ hashing/num_of_element.cpp
@@ -0,0 +1,25 @@
+//hashing is one of the way to find the  number of element
+//in the array
+#include<iostream>
+using namespace  std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int hash[12]={0};
+    for(int i=0;i<n;i++){
+        hash[arr[i]]+=1;
+    }
+
+    int q;
+    cin>>q;
+    while(q--){
+        int num;
+        cin>>num;
+        cout<<hash[num]<<endl;
+    }
+    return 0;
+}
