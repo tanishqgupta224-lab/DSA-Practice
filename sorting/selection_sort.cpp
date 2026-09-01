db491be3b516d7f23d461dcacbca11a228993929
Tanishq Gupta <tanishqgupta224@gmail.com>
2026-09-02 01:07:29 +0530

    praactice

---
+++ sorting/selection_sort.cpp
@@ -0,0 +1,36 @@
+//sorting  a number using a selection sort method
+
+#include<iostream>
+using namespace std;
+void sort(int arr[],int n){
+    for(int i=0;i<n-2;i++){
+        int min=i;
+        for(int j=i;j<n-1;j++){
+            if(arr[j]<arr[min]){
+                min=j;
+                
+            }
+            
+        }
+        //swap
+        int temp;
+        temp=arr[min];
+        arr[min]=arr[i];
+        arr[i]=temp;
+    }
+
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
