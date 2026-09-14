c80b9f60abc763fc0e2d5fc8ebfbda8b04012191
Tanishq Gupta <tanishqgupta224@gmail.com>
2026-09-15 02:34:53 +0530

    practice

---
+++ arrays/RightRotateBy1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace  std;
+int main(){
+    vector<int>arr={1,2,3,4,5,6};
+    int n=arr.size();
+    int temp=arr[n-1];
+    for(int i=n-1;i>0;i--){
+        arr[i]=arr[i-1];
+    }
+    arr[0]=temp;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
